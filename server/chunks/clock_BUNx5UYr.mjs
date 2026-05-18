540f016ac1a5c4456d8ef4ae7f105ff859b36f8f
BapeCode <BapeCode@users.noreply.github.com>
2026-05-18 17:46:23 +0000

    deploy: 79b2bcf7f565326d6bc6f4ff0d9f82246742bf42

--- server/chunks/clock_BUNx5UYr.mjs
+++
@@ -1,5 +0,0 @@
-import { d as createLucideIcon } from './Footer_Y9EJItiz.mjs';
-
-const Clock = createLucideIcon("clock", [["circle", { "cx": "12", "cy": "12", "r": "10" }], ["path", { "d": "M12 6v6l4 2" }]]);
-
-export { Clock as C };
--- server/entry.mjs
+++
@@ -1,8 +0,0 @@
-export { l as handler, o as options, t as startServer } from './chunks/server_DSd120XH.mjs';
-import '@astrojs/internal-helpers/path';
-import '@astrojs/internal-helpers/remote';
-import 'piccolore';
-import 'es-module-lexer';
-import 'clsx';
-import '@astrojs/internal-helpers/object';
-import 'html-escaper';
